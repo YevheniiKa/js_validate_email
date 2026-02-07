5b2c9cdb51aa7545642f3e093b2d58dbe40a6762
Yevhenii Kaliaiev <eugenyr34@gmail.com>
2026-02-07 22:40:43 +0200

    changed gmail.com -> mail.com to align with task description

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -10,19 +10,19 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`should return boolean`, () => {
-    expect(typeof validateEmail("test@gmail.com")).toBe("boolean");
+    expect(typeof validateEmail("test@mail.com")).toBe("boolean");
   });
 
   it(`valid email with digits`, () => {
-    expect(validateEmail("test838@gmail.com")).toBeTruthy();
+    expect(validateEmail("test838@mail.com")).toBeTruthy();
   });
 
   it(`valid email with character "_"`, () => {
-    expect(validateEmail("test_one@gmail.com")).toBeTruthy();
+    expect(validateEmail("test_one@mail.com")).toBeTruthy();
   });
 
   it(`valid email with character "-"`, () => {
-    expect(validateEmail("test-two@gmail.com")).toBeTruthy();
+    expect(validateEmail("test-two@mail.com")).toBeTruthy();
   });
 
   it(`valid email with one letter in personal_info and a domain`, () => {
@@ -34,19 +34,19 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`not-valid email with not allowed chars like "! $ % & ' * + / = ? ^ { | } ~"`, () => {
-    expect(validateEmail("t$est!t&#w=o@gmail.com")).toBeFalsy();
+    expect(validateEmail("t$est!t&#w=o@mail.com")).toBeFalsy();
   });
 
   it(`not-valid email with double dots ".."`, () => {
-    expect(validateEmail("tte..st@gmail.com")).toBeFalsy();
+    expect(validateEmail("tte..st@mail.com")).toBeFalsy();
   });
 
   it(`not-valid email with not only Latin letters`, () => {
-    expect(validateEmail("test-тест@gmail.com")).toBeFalsy();
+    expect(validateEmail("test-тест@mail.com")).toBeFalsy();
   });
 
   it(`not-valid email without @`, () => {
-    expect(validateEmail("test.gmail.com")).toBeFalsy();
+    expect(validateEmail("test.mail.com")).toBeFalsy();
   });
 
   it(`not-valid email without domain`, () => {
@@ -54,14 +54,14 @@ describe(`Function 'validateEmail':`, () => {
   });
 
   it(`not-valid email where personal_info start from dot "."`, () => {
-    expect(validateEmail(".test@gmail.com")).toBeFalsy();
+    expect(validateEmail(".test@mail.com")).toBeFalsy();
   });
 
   it(`not-valid email where personal_info ends with "."`, () => {
-    expect(validateEmail("test.@gmail.com")).toBeFalsy();
+    expect(validateEmail("test.@mail.com")).toBeFalsy();
   });
 
   it(`not-valid email where domain start from dot "."`, () => {
-    expect(validateEmail("test@.gmail.com")).toBeFalsy();
+    expect(validateEmail("test@.mail.com")).toBeFalsy();
   });
 });
