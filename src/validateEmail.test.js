f86083eefd74620f3ca16d3eca6f229c6fb9d9f0
Yevhenii Kaliaiev <eugenyr34@gmail.com>
2026-02-07 22:33:06 +0200

    added solution to the task

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -1,20 +1,59 @@
-'use strict';
+/* eslint-disable max-len */
+/* eslint-disable quotes */
+"use strict";
 
 describe(`Function 'validateEmail':`, () => {
-  const validateEmail = require('./validateEmail');
+  const validateEmail = require("./validateEmail");
 
   it(`should be declared`, () => {
     expect(validateEmail).toBeInstanceOf(Function);
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof validateEmail("test@gmail.com")).toBe("boolean");
   });
 
-  it(`should return 'true' for the valid email`, () => {
-    expect(validateEmail('test838@gmail.com.'))
-      .toBeTruthy();
+  it(`valid email with digits`, () => {
+    expect(validateEmail("test838@gmail.com")).toBeTruthy();
   });
 
-  // write more tests here
+  it(`valid email with character "_"`, () => {
+    expect(validateEmail("test_one@gmail.com")).toBeTruthy();
+  });
+
+  it(`valid email with character "-"`, () => {
+    expect(validateEmail("test-two@gmail.com")).toBeTruthy();
+  });
+
+  it(`valid email with one letter in personal_info and a domain`, () => {
+    expect(validateEmail("t@q.c")).toBeTruthy();
+  });
+
+  it(`not-valid email with not allowed chars like "! $ % & ' * + / = ? ^ { | } ~"`, () => {
+    expect(validateEmail("t$est!t&#w=o@gmail.com")).toBeFalsy();
+  });
+
+  it(`not-valid email with double dots ".."`, () => {
+    expect(validateEmail("tte..st@gmail.com")).toBeFalsy();
+  });
+
+  it(`not-valid email with not only Latin letters`, () => {
+    expect(validateEmail("test-тест@gmail.com")).toBeFalsy();
+  });
+
+  it(`not-valid email without @`, () => {
+    expect(validateEmail("test.gmail.com")).toBeFalsy();
+  });
+
+  it(`not-valid email without domain`, () => {
+    expect(validateEmail("false@email")).toBeFalsy();
+  });
+
+  it(`not-valid email where personal_info start from dot "."`, () => {
+    expect(validateEmail(".test@gmail.com")).toBeFalsy();
+  });
+
+  it(`not-valid email where domain start from dot "."`, () => {
+    expect(validateEmail("test@.gmail.com")).toBeFalsy();
+  });
 });
